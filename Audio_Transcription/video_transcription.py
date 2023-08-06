04c4f6d873e0d5a10dd20e154bfaa2201ca58b78
mike24dzy <mike24dzy@gmail.com>
2023-08-06 19:18:31 +0800

    Fixed issues with whisper misalignment and bugs

--- Audio_Transcription/video_transcription.py
+++ Audio_Transcription/video_transcription.py
@@ -10,6 +10,8 @@ import whisper
 from whisperx import load_align_model, align
 from whisperx.diarize import DiarizationPipeline, assign_word_speakers
 import matplotlib.pyplot as plt
+from datetime import datetime
+import json
 
 # Download Video Through YouTube
 def download_youtube_video(url: str, output_path: Optional[str] = None) -> None:
@@ -76,17 +78,20 @@ def assign_speakers(diarization_result: Dict[str, Any], aligned_segments: Dict[s
     # )
     
     result_segments = assign_word_speakers(diarization_result, aligned_segments)
-    print("result_segments", result_segments)
+    # print("result_segments", result_segments)
     results_segments_w_speakers: List[Dict[str, Any]] = []
 
     segments = result_segments["segments"]
+    # print("segments from result_segments:\n", segments)
     for i in segments:
+        # print("\nprint i", i)
+        speaker = i["speaker"] if "speaker" in i else "Unknown"
         results_segments_w_speakers.append(
             {
                 "start": i["start"],
                 "end": i["end"],
                 "text": i["text"],
-                "speaker": i["speaker"],
+                "speaker": speaker,
             }
         )
     # for result_segment in result_segments["segments"]:
@@ -102,22 +107,30 @@ def assign_speakers(diarization_result: Dict[str, Any], aligned_segments: Dict[s
 
 # Putting everything together, transcribe and diarize
 # Returns: A list of dictionaries representing each segment of the transcript, including the start and end times, the spoken text, and the speaker ID.
-def transcribe_and_diarize(audio_file: str, hf_token: str, model_name: str, device: str = "cpu") -> List[Dict[str, Any]]:
-
-    transcript = transcribe(audio_file, model_name, device)
-    print("Transcription is done!")
+def whisperx_align_and_diarize(audio_file: str, transcript: Dict[str, Any], hf_token: str, output_file: str, device: str = "cpu") -> List[Dict[str, Any]]:
 
     aligned_segments = align_segments(transcript["segments"], transcript["language_code"], audio_file, device)
-    print("aligned_segments", aligned_segments)
+    # print("aligned_segments", aligned_segments)
+    print("Aligned segments!")
 
     diarization_result = diarize(audio_file, hf_token)
+    # print("Diarization is done!\n", diarization_result)
     print("Diarization is done!")
 
     results_segments_w_speakers = assign_speakers(diarization_result, aligned_segments)
     print("Speaker assignment is done!")
 
     # Instead of printing out the resutls, let's save the result in a text file
-    with open("/Users/mike24dzy/Downloads/test.txt", "w") as f:
+    # with open("/Users/mike24dzy/Downloads/test.txt", "w") as f:
+    #     for i, segment in enumerate(results_segments_w_speakers):
+    #         f.write(f"Segment {i + 1}:\n")
+    #         f.write(f"Start time: {segment['start']:.2f}\n")
+    #         f.write(f"End time: {segment['end']:.2f}\n")
+    #         f.write(f"Speaker: {segment['speaker']}\n")
+    #         f.write(f"Transcript: {segment['text']}\n")
+    #         f.write("\n")
+    # print("The result is saved into Download folder!")
+    with open(output_file, "w") as f:
         for i, segment in enumerate(results_segments_w_speakers):
             f.write(f"Segment {i + 1}:\n")
             f.write(f"Start time: {segment['start']:.2f}\n")
@@ -141,21 +154,36 @@ if __name__ == "__main__":
     hf_token = os.environ["HUGGINGFACE_TOKEN"]
     language_code = "en"
 
-    audio_file = "resistance_converted.wav"
+    # audio_file = "resistance_converted.wav"
+    audio_file = "/Users/mike24dzy/Downloads/William.wav"
+    output_file = "../Transcripts/output_William.txt"
     ## Remember to convert the audio file if ran into pyannote error: "ffmpeg -i input.wav -c:a pcm_s16le input_converted.wav"
 
     # Run the transcription and diarization
     start_time = time.time()
-    print("Start at: ", start_time)
+    start_time_datetime = datetime.fromtimestamp(start_time).strftime("%Y-%m-%d %H:%M:%S")
+    print("Start at: ", start_time_datetime)
 
-    # Result is saved to the download folder and also is here
-    results_segments_w_speakers = transcribe_and_diarize(audio_file, hf_token, "large", "cpu")
+    # Transcribe audio
+    transcript = transcribe(audio_file, 'large', "cpu")
+    print("Transcription is done!\n")
+
+    ## Temperarily save the transcription result into json so don't need to run it every time
+    with open('transcription_temp.json', 'w') as f:
+        json.dump(transcript, f)
+    print("Transcript saved to json!\n")
+    
+    ## Load transcript back in for debugging
+    # with open('transcription_temp.json', 'r') as f:
+    #     transcript = json.load(f)
+
+    # Align transcript, diarize, assign speakers, and save the result to a text file
+    results_segments_w_speakers = whisperx_align_and_diarize(audio_file, transcript, hf_token, output_file, "cpu")
     
     end_time = time.time()
-    print("End at: ", end_time)
+    end_time_datetime = datetime.fromtimestamp(end_time).strftime("%Y-%m-%d %H:%M:%S")
+    print("End at: ", end_time_datetime)
 
     memory_usage = psutil.Process().memory_info().rss / (1024 ** 3)
     print("Memory Usage: ", memory_usage)
-
-
     
